2650c1fb62b053ce0e8135aa4031e1ed31c15b26
a00889404 <a00889404@SE12L0306-12.ad.bcit.ca>
2016-01-28 13:25:00 -0800

    feature 1 enhanced

---
+++ application/controllers/First.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * Our homepage. Show a table of all the author pictures. Clicking on one should show their quote.
+ * Our quotes model has been autoloaded, because we use it everywhere.
+ * 
+ * controllers/Welcome.php
+ *
+ * ------------------------------------------------------------------------
+ */
+class First extends Application {
+
+	function __construct()
+	{
+		parent::__construct();
+	}
+
+	//-------------------------------------------------------------
+	//  The normal pages
+	//-------------------------------------------------------------
+
+	function index()
+	{
+		$this->data['pagebody'] = 'justone';	// this is the view we want shown
+		// build the list of authors, to pass on to our view
+		$source = $this->quotes->first();
+                $this->data = array_merge($this->data, $source);
+                
+//		$authors = array();
+//		foreach ($source as $record)
+//		{
+//			$authors[] = array('who' => $record['who'], 'mug' => $record['mug'], 'href' => $record['where']);
+//		}
+//		$this->data['authors'] = $authors;
+//
+		$this->render();
+	}
+
+}
+
+/* End of file Welcome.php */
+/* Location: application/controllers/Welcome.php */
